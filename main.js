1695c231ec30bec10b27ea463347955ddbd9aed5
DrgnInventor <62597889+DrgnInventor@users.noreply.github.com>
2020-11-11 18:23:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector(".signup form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log("Submit was pressed!");
+});
+function goToSignUpForm() {
+    document.querySelector(".signup").scrollIntoView();
+
+}
